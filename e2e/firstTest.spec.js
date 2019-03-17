92a2a6b895dd4e7181284f05ab0b4a4fa49cce72
cborjah <cpborja32@gmail.com>
2019-03-17 12:51:21 -0700

    moves App.js into src folder, fixes detox binaryPath config, welcome screen test now passes

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -2,4 +2,8 @@ describe('Example', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('welcome'))).toBeVisible();
+  });
 });
